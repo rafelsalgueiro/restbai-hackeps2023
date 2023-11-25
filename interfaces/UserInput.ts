0019de5a6f446164b407352f20abbec1a3aa67e8
peremunoz <peremunozfiguerol33@gmail.com>
2023-11-25 16:16:56 +0100

    Export user info interface

--- interfaces/UserInput.ts
+++ interfaces/UserInput.ts
@@ -1,15 +1,15 @@
-import { PropertyType } from "@/enums/PropertyType.enum";
+import { PropertyType } from '@/enums/PropertyType.enum'
 
-interface UserInput{
-    street: string;
-    zipcode?: number;
-    m2: number;
-    city: string;
-    state: string;
-    country: string;
-    propertyType: PropertyType,
-    garageSpaces?: number,
-    yearBuilt?: number;
-    tone: "professional",
-    images: string[]
+export interface UserInput {
+  street: string
+  zipcode?: number
+  m2: number
+  city: string
+  state: string
+  country: string
+  propertyType: PropertyType
+  garageSpaces?: number
+  yearBuilt?: number
+  tone: 'professional'
+  images: string[]
 }
