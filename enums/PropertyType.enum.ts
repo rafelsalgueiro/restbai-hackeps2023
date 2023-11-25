cc6f67997ecafefe02c8c3e40e97ede92bbd2327
Andreu Pérez <apt15udl@gmail.com>
2023-11-25 15:13:00 +0100

    Create PropertyType.enum

---
+++ enums/PropertyType.enum.ts
@@ -0,0 +1,14 @@
+export enum PropertyType {
+    apartment,
+    condominium,
+    duplex,
+    manufactured_home,
+    mobile_home,
+    multi_family,
+    quadruplex,
+    ranch,
+    residential,
+    single_family_residence,
+    townhouse,
+    triplex,
+}
